7f3434aaaf532a8f933b2b4fc096401402eb4644
CaneCilia <kanekash23@gmail.com>
2026-09-17 19:11:46 +0530

    chore: integrate AuthProvider into app root (Phase 3.2)

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { SQLiteProvider } from 'expo-sqlite';
 import { AppNavigator } from './src/navigation/AppNavigator';
 import { initializeDatabase } from './src/database/db';
+import { AuthProvider } from './src/contexts/AuthContext';
 
 export default function App() {
   return (
-    <SQLiteProvider databaseName="dayframe.db" onInit={initializeDatabase}>
-      <AppNavigator />
-    </SQLiteProvider>
+    <AuthProvider>
+      <SQLiteProvider databaseName="dayframe.db" onInit={initializeDatabase}>
+        <AppNavigator />
+      </SQLiteProvider>
+    </AuthProvider>
   );
 }
